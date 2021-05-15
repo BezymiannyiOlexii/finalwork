53f119574870f8a61a0f4521c21ff9646676be1c
BezymiannyiOlexii <genhuly@gmail.com>
2021-05-15 03:34:52 +0300

    some

--- app.py
+++ app.py
@@ -5,8 +5,21 @@ from employees import *
 from sales import *
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == "POST":
+        name = request.form['name']
+        password = request.form['password']
+        usr = db.session.execute(f"SELECT password from Users where name='{name}'").fetchall()
+        if int(password) == int(usr[0][0]):
+            return redirect('/main')
+        else:
+            return "Доступ не разрешен"
+    else:
+        return render_template("enter.html")
+
+@app.route('/main')
+def main():
     return render_template("main_page.html")
 
 
--- classes_file.py
+++ classes_file.py
@@ -25,6 +25,7 @@ class Products(db.Model):
     def __repr__(self):
         return 'Products %r' % self.id_product
 
+
 class Orders(db.Model):
     id_order = db.Column(db.Integer, primary_key=True)
     id_product = db.Column(db.Integer, nullable=False)
@@ -37,6 +38,7 @@ class Orders(db.Model):
     def __repr__(self):
         return 'Order %r' % self.id_order
 
+
 class Employees(db.Model):
     id_employee = db.Column(db.Integer, primary_key=True)
     name_employee = db.Column(db.String(30), nullable=False)
@@ -45,6 +47,7 @@ class Employees(db.Model):
 
     def __repr__(self):
         return 'Employee %r' % self.id_employee
+
 
 class Sales(db.Model):
     id_sale = db.Column(db.Integer, primary_key=True)
@@ -60,5 +63,10 @@ class Sales(db.Model):
         return 'Sales %r' % self.id_sale
 
 
+class Users(db.Model):
+    id_user = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.Integer, nullable=False)
 
-
+    def __repr__(self):
+        return 'Users %r' % self.id_user
